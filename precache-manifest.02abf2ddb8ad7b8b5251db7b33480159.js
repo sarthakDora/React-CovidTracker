cd14b08553664a16fc6058a20041f80e59b558d4
Sarthak Pathak <sarthak1@umbc.edu>
2020-08-06 22:05:52 -0400

    Updates

---
+++ precache-manifest.02abf2ddb8ad7b8b5251db7b33480159.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5198021095880832e0e172d2d5e6c6",
+    "url": "/React-CovidTracker/index.html"
+  },
+  {
+    "revision": "c76f4516ad0d302d2685",
+    "url": "/React-CovidTracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "c24a156ffe0b715bcf8e",
+    "url": "/React-CovidTracker/static/css/main.aa1b96df.chunk.css"
+  },
+  {
+    "revision": "c76f4516ad0d302d2685",
+    "url": "/React-CovidTracker/static/js/2.47e53bc9.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/React-CovidTracker/static/js/2.47e53bc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c24a156ffe0b715bcf8e",
+    "url": "/React-CovidTracker/static/js/main.bedd73f5.chunk.js"
+  },
+  {
+    "revision": "7c72f1be79588c77f06f",
+    "url": "/React-CovidTracker/static/js/runtime-main.fd4df977.js"
+  }
+]);
